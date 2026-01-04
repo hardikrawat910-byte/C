870a9b10f90c36b64e573790bfdd8a7458555692
hardikrawat910-byte <hardikrawat910@gmail.com>
2026-01-04 21:14:05 +0530

    Refactor program to perform basic arithmetic operations

--- prog1.c
+++ prog1.c
@@ -1,11 +1,31 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <conio.h>
 
-int main()
-
-{	int a,b,sum;
-	printf("Enter two numbers:");
-	scanf("%d %d", &a, &b);
- 	sum = a+b;
-	printf("Sum = %d", sum);
-	return 0;
+int main() {
+    int a, b;
+    char op;
+    clrscr();
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    printf("Enter operator (+, -, etc.): ");
+    scanf(" %c", &op);  // Space before %c skips newline
+    switch(op) {
+        case '+':
+            printf("%d + %d = %d\n", a, b, a + b);
+            break;
+        case '-':
+            printf("%d - %d = %d\n", a, b, a - b);
+            break;
+        case '*':
+            printf("%d * %d = %d\n", a, b, a * b);
+            break;
+        case '/':
+            if (b != 0) printf("%d / %d = %d\n", a, b, a / b);
+            else printf("Division by zero!\n");
+            break;
+        default:
+            printf("Invalid operator!\n");
+    }
+    getch();
+    return 0;
 }
